f983d6d17b4e243f09c20c1aa3b643639adc66b8
Cristian Morano <Cristian.morano@hotmail.com>
2025-03-25 18:17:29 +0100

    CV, version 1.2

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -74,7 +74,7 @@ const AboutSection: React.FC = () => {
                     </div>
                     <div className="group">
                         <a
-                            href="/CV_personal.pdf"
+                            href="/CV Cristian Morano.pdf"
                             download="CV_Cristian_Morano.pdf"
                             title="Descargar CV"
                             className="mt-8 inline-block bg-gradient-to-br from-black to-blue-950 border border-cyan-400/30 rounded-full shadow-lg shadow-blue-500/40 hover:shadow-blue-500/80 transition-all duration-300 cursor-pointer px-4 py-2 gradient-text"
